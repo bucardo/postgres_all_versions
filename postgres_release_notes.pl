45755b5360a7515cda7d1ff91108f012c4b891eb
Greg Sabino Mullane <greg@turnstep.com>
2022-11-11 09:48:00 -0500

    Version 10 is now EOL

--- postgres_release_notes.pl
+++ postgres_release_notes.pl
@@ -14,8 +14,8 @@ our $VERSION = '1.31';
 my $USAGE = "$0 [--noindexcache] [--nocache] [--verbose]";
 
 my $EOLURL = 'https://www.postgresql.org/support/versioning/';
-my $EOL = '9.6';
-my $EOLSOON = '10';
+my $EOL = '10';
+my $EOLSOON = '0';
 my $CURRENT_VERSION = 15;
 my $EOLDATES = q{
 15 November 11, 2027
